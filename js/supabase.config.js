0b698d73f82d20fd7b077b367fafa2d37c9dd517
illili091027-sys <illili091027@gmail.com>
2026-09-03 15:29:56 +0900

    Update supabase.config.js

--- js/supabase.config.js
+++ js/supabase.config.js
@@ -2,6 +2,6 @@
 // The "publishable" key is designed to be visible in a browser. Never use
 // the service_role key here.
 window.SUPABASE_CONFIG = {
-  url: 'https://olwfrcobwgvwweixklady.supabase.co',
-  anonKey: 'sb_publishable_wnulqT8ugtMoEG2foqt-7Q_krStnSK1'
+  url: 'https://olwfrcobwgwweixklady.supabase.co',
+  anonKey: 'sb_publishable_wnuIqT8ugtMoEG2foqt-7Q_krSth8K1'
 };
